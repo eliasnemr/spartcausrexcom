c688424fc5cef160c3f0262985a0e761a7c30cdd
Elias <eliasnemr@gmail.com>
2023-10-16 19:01:22 +0300

    Dropdown menu hooked up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 import Grid from "./Components/UI/Grid";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import "flowbite";
+import { Dropdown } from "flowbite";
 
 const App = () => {
   const [displayMenu, setDisplayMenu] = useState(false);
   const navigate = useNavigate();
+
+  const $targetEl = document.getElementById("dropdownNavbar");
+
+  const $triggerEl = document.getElementById("dropdownButton");
+
+  const dropdown = new Dropdown($targetEl, $triggerEl);
 
   return (
     <Grid
@@ -55,9 +63,10 @@ const App = () => {
             </NavLink>
           </nav>
 
-          {!displayMenu && (
+          <div className="relative md:hidden">
             <svg
-              onClick={() => setDisplayMenu(true)}
+              id="dropdownButton"
+              data-dropdown-toggle="dropdownNavbar"
               className="block md:hidden"
               xmlns="http://www.w3.org/2000/svg"
               height="24"
@@ -66,19 +75,75 @@ const App = () => {
             >
               <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
             </svg>
-          )}
-          {!!displayMenu && (
-            <svg
-              onClick={() => setDisplayMenu(false)}
-              className="block md:hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              height="24"
-              viewBox="0 -960 960 960"
-              width="24"
+            <nav
+              id="dropdownNavbar"
+              className=" z-10 hidden bg-slate-900 divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
             >
-              <path d="M120-240v-80h520v80H120Zm664-40L584-480l200-200 56 56-144 144 144 144-56 56ZM120-440v-80h400v80H120Zm0-200v-80h520v80H120Z" />
-            </svg>
-          )}
+              <div className="flex flex-col gap-4 p-4">
+                <NavLink
+                  id="dropdownButton"
+                  onClick={() => dropdown.hide()}
+                  to="/"
+                  className={`${({ isActive }) =>
+                    isActive
+                      ? "active"
+                      : ""} text-slate-100 hover:text-white  hover:cursor-pointer`}
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  id="dropdownButton"
+                  onClick={() => dropdown.hide()}
+                  to="/runanode"
+                  className={`${({ isActive }) =>
+                    isActive
+                      ? "active"
+                      : ""} text-slate-100 hover:text-white  hover:cursor-pointer`}
+                >
+                  Run a node
+                </NavLink>
+                <NavLink
+                  id="dropdownButton"
+                  onClick={() => dropdown.hide()}
+                  to="/downloads"
+                  className={`${({ isActive }) =>
+                    isActive
+                      ? "active"
+                      : ""} text-slate-100 hover:text-white hover:cursor-pointer`}
+                >
+                  Downloads
+                </NavLink>
+                <NavLink
+                  id="dropdownButton"
+                  onClick={() => dropdown.hide()}
+                  to="/blog"
+                  className={`${({ isActive }) =>
+                    isActive
+                      ? "active"
+                      : ""} text-slate-100 hover:text-white hover:cursor-pointer`}
+                >
+                  Blog
+                </NavLink>
+              </div>
+            </nav>
+          </div>
+          {/* {!displayMenu && (
+          )} */}
+          {/* {!!displayMenu && (
+            <div className="dropdown">
+              <svg
+                onClick={() => setDisplayMenu(false)}
+                className="block md:hidden"
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 -960 960 960"
+                width="24"
+              >
+                <path d="M120-240v-80h520v80H120Zm664-40L584-480l200-200 56 56-144 144 144 144-56 56ZM120-440v-80h400v80H120Zm0-200v-80h520v80H120Z" />
+              </svg>
+              
+            </div>
+          )} */}
         </div>
       }
     >
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {},
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
